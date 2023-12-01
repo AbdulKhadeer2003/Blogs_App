7fdfd3e35e635ec6b01bcfa9afc6c3b06f75a23f
AbdulKhadeer <786sak47@gmail.com>
2023-12-02 02:28:33 +0530

    Version-2

--- app.js
+++ app.js
@@ -4,37 +4,48 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 main();
 async function main(){
 await mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
 }
-
+const UserData={
+  userName: String,
+  password: String
+}
 const postSchema = {
+  userName: String,
+  userId: String,
   title: String,
   content: String,
   time: Date
 };
-
+const Userdata=mongoose.model("UserData",UserData);
 const Post = mongoose.model("Post", postSchema);
 
-app.get("/", async function(req, res){
 
+app.get("/",(req,res)=>{
+  res.render("login",{showAlert:false});
+});
+
+app.get("/login",(req,res)=>{
+  res.render("login",{showAlert:false});
+});
+
+app.get("/register",(req,res)=>{
+  res.render("register");
+});
+
+app.get("/home",async (req,res)=>{
   await Post.find({})
   .then( posts =>{
-      res.render("home", {
-        startingContent: homeStartingContent,
-        posts: posts
-      });
+      res.render("home", {posts: posts});
   })
   .catch(err=>console.log(err));
 });
@@ -42,6 +53,48 @@ app.get("/", async function(req, res){
 app.get("/compose", function(req, res){
   res.render("compose");
 });
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+
+app.post("/register", async (req,res)=>{
+  const user=new Userdata({
+      userName: req.body.username,
+      password: req.body.password
+  });
+  await user.save();
+  res.redirect("/home");
+});
+
+app.post("/login",(req,res)=>{
+  const { username, password } = req.body;
+  console.log(username);
+  console.log(password);
+  Userdata.findOne({ userName: username })
+  .then((user) => {
+      // Check if the user object exists
+
+      if (user.password === password) {
+        res.redirect("/home");
+      } else {
+        console.log("error");
+        res.render('login',{showAlert: true});
+
+      }
+    })
+  .catch((error) => {
+    console.log(error);
+        res.render('login',{showAlert: true});
+  });
+
+});
+
 
 app.post("/compose", async function(req, res){
   const post = new Post({
@@ -51,12 +104,12 @@ app.post("/compose", async function(req, res){
   });
   
   await post.save();
-  res.redirect('/');
+  res.redirect('/home');
 });
 
 app.get("/posts/:postId", function(req, res){
 
-const requestedPostId = req.params.postId;
+  const requestedPostId = req.params.postId;
 
   Post.findById(requestedPostId)
   .then(post =>{
@@ -69,13 +122,7 @@ const requestedPostId = req.params.postId;
 
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
-});
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
 
 
 app.listen(3000, function() {
